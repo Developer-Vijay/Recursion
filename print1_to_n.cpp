675a2d64380a2895a68ac28cbc2a3a32bc969362
Developer-Vijay <vijaysahoo147@gmail.com>
2022-04-23 23:03:29 +0530

    Updating

---
+++ print1_to_n.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void print(int n)
+{
+    if(n==1)
+    {
+        return ;
+    }
+
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main(){
+
+print(10);
+    return 0;
+}
